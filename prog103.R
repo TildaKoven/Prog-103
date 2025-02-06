701e1a95a88a7d83a934f637faa58e02fd0ea91a
TildaKoven <tildakoven@ucsb.edu>
2025-02-06 09:20:06 -0800

    Prog103
    done and dusted

--- prog103.R
+++ prog103.R
@@ -1,5 +1,5 @@
 library(marinecs100b)
-
+remotes::install_github("MarineCS-100B/marinecs100b")
 
 # Review: write a function ------------------------------------------------
 
@@ -18,50 +18,138 @@ hours_cold <- n_cold * 30 / 60
 days_total <- n_total * 30 / 60 / 24
 hours_cold_per_day <- hours_cold / days_total
 hours_cold_per_day
+# The code takes the data from a site and season and determines how many hours
+# per day the temperature was less than negative four degrees
 
 # P2 Let's turn that code chunk into a function. What would you call that
 # function? How many parameters should it take and what would you call them?
+# I would call the function cold_hours_day
+#the parameters are the site and season
 
 # P3 Write a function to encapsulate the code chunk above. Check that it
 # contains all five parts of a function.
-
-
+cold_hours_day <- function(site, season) {
+  n_cold <- sum(kefj_site == site &
+                  kefj_season == season &
+                  kefj_temperature <= -4 &
+                  kefj_exposure == "air")
+  n_total <- sum(kefj_site == site &
+                   kefj_season == season)
+  hours_cold <- n_cold * 30 / 60
+  days_total <- n_total * 30 / 60 / 24
+  hours_cold_per_day <- hours_cold / days_total
+  return(hours_cold_per_day)
+}
+cold_hours_day("Nuka_Pass", "Late winter")
 # Make an extreme choice --------------------------------------------------
 
 # P4 Fill in the code below to create a logical vector indicating extreme
 # temperatures.
 
-extreme_type <- "cold"
-if (??? == "???") {
-  is_extreme <- kefj_temperature ??? ???
-} ??? {
-  ???
+extreme_type <- "hot"
+if (extreme_type == "cold") {
+  is_extreme <- kefj_temperature <= -4
+} else if (extreme_type == "hot") {
+  is_extreme <- kefj_temperature >= 25
 }
+
+is_extreme
 
 # P5 Copy-paste the code from P1 and edit it to incorporate the is_extreme
 # vector into the extreme temperature exposure procedure.
+site <- "Nuka_Pass"
+season <- "Late winter"
+n_extreme <- sum(kefj_site == site &
+                kefj_season == season &
+                is_extreme &
+                kefj_exposure == "air" & is_extreme)
+n_total <- sum(kefj_site == site &
+                 kefj_season == season)
+hours_extreme <- n_extreme * 30 / 60
+days_total <- n_total * 30 / 60 / 24
+hours_extreme_per_day <- hours_extreme / days_total
+hours_extreme_per_day
 
 # P6 Copy-paste the function you wrote in P3 and edit it to add a parameter that
 # lets you switch between extreme heat and cold exposure.
-
-
+extreme_hours_day <- function(site, season, hotcold) {
+  if (hotcold == "cold") {
+  n_extreme <- sum(kefj_site == site &
+                  kefj_season == season &
+                  kefj_temperature <= -4 &
+                  kefj_exposure == "air")
+  } else if (hotcold == "hot") {
+    n_extreme <- sum(kefj_site == site &
+                       kefj_season == season &
+                       kefj_temperature >= 25 &
+                       kefj_exposure == "air")
+  }
+  n_total <- sum(kefj_site == site &
+                   kefj_season == season)
+  hours_extreme <- n_extreme * 30 / 60
+  days_total <- n_total * 30 / 60 / 24
+  hours_extreme_per_day <- hours_extreme / days_total
+  return(hours_extreme_per_day)
+}
+extreme_hours_day("Nuka_Pass", "Summer", "hot")
 # Season to taste ---------------------------------------------------------
 
 # P7 What seasons are in the kefj dataset? What function would you use to
 # identify them?
+#The seasons are spring, summer, fall, early winter, and late winter
+#I would just run kefj_season in the console, it tells you the levels
 
 # P8 Fill in the blanks below to make a for loop that prints the extreme hot and
 # cold exposure across seasons at site Aialik.
 
-seasons <- ???
-  for (??? in ???) {
-    heat_exposure <- ???(???, ???, "hot")
-    cold_exposure <- ???
-    print(paste("Aialik", ???, heat_exposure, cold_exposure))
+seasons <- c("Late winter", "Spring", "Summer", "Fall", "Early winter")
+  for (season in seasons) {
+    heat_exposure <- extreme_hours_day("Aialik", season, "hot")
+    cold_exposure <- extreme_hours_day("Aialik", season, "cold")
+    print(paste("Aialik", season, heat_exposure, cold_exposure))
 }
+#"Aialik Late winter 0 0.0136632200886263"
+# "Aialik Spring 0.0169398907103825 0.00273224043715847"
+# "Aialik Summer 0.114288330776804 0"
+# "Aialik Fall 0.0105072463768116 0"
+# "Aialik Early winter 0 0.0109289617486339"
 
 # P9 Copy-paste your answer to P8 and add a nested for loop to iterate across
 # sites as well as seasons.
-
+seasons <- unique(kefj_season)
+sites <- unique(kefj_site)
+for (season in seasons) {
+  for (site in sites) {
+  heat_exposure <- extreme_hours_day(site, season, "hot")
+  cold_exposure <- extreme_hours_day(site, season, "cold")
+  print(paste(site, season, heat_exposure, cold_exposure))
+  }
+}
+#"Aialik Summer 0.114288330776804 0"
+# "Harris Summer 0.00205189586628479 0"
+# "McCarty Summer 0.000549111126364198 0"
+# "Nuka_Bay Summer 0.0143900577782623 0"
+# "Nuka_Pass Summer 0.0713972741738348 0"
+# "Aialik Fall 0.0105072463768116 0"
+# "Harris Fall 0 0"
+# "McCarty Fall 0.00036231884057971 0"
+# "Nuka_Bay Fall 0 0"
+# "Nuka_Pass Fall 0.0166666666666667 0"
+# "Aialik Early winter 0 0.0109289617486339"
+# "Harris Early winter 0 0.00922131147540984"
+# "McCarty Early winter 0 0.00655737704918033"
+# "Nuka_Bay Early winter 0 0.0126103404791929"
+# "Nuka_Pass Early winter 0 0.0202185792349727"
+# "Aialik Late winter 0 0.0136632200886263"
+# "Harris Late winter 0 0.0221453287197232"
+# "McCarty Late winter 0 0.0228796358939664"
+# "Nuka_Bay Late winter 0 0.0255759018169547"
+# "Nuka_Pass Late winter 0 0.060930576070901"
+# "Aialik Spring 0.0169398907103825 0.00273224043715847"
+# "Harris Spring 0 0.00512295081967213"
+# "McCarty Spring 0 0.00218579234972678"
+# "Nuka_Bay Spring 0 0.00441361916771753"
+# "Nuka_Pass Spring 0.00819672131147541 0.00109289617486339"
 # P10 Examine your results from P9. You should find two outputs where both
 # extreme heat and cold exposure were 0. What season were they in?
+#They were both in Fall
--- prog103exercises.R
+++ prog103exercises.R
@@ -15,16 +15,35 @@ dna2 <- "attattaggaacaca"
 # Use them to print whether or not the following statements are TRUE or FALSE.
 #
 # w is greater than 10
+w>10
+# TRUE
 # "green" is in colors
+"green"%in%colors
+#TRUE
 # x is greater than y
+x>y
+#FALSE
 # Each value in masses is greater than 40.
+masses > 40
+#TRUE FALSE FALSE  TRUE  TRUE
 # 2 * x + 0.2 is equal to y
+y == 2*x + 0.2
+#FALSE
 # dna1 is the same as dna2
+dna1==dna2
+#FALSE
 # dna1 is not the same as dna2
+dna1 != dna2
+#TRUE
 # w is greater than x, or y is greater than z
+w>x | y>z
+#TRUE
 # x times w is between 13.2 and 13.5
+13.2 < x*w & x*w < 13.5
+#TRUE
 # Each mass in masses is between 30 and 50.
-
+masses >30 & masses <50
+#TRUE TRUE FALSE FALSE TRUE -> no
 
 # Making choices with if, else, and else if -------------------------------
 
@@ -32,35 +51,55 @@ dna2 <- "attattaggaacaca"
 # it sets y <- 10.
 
 age_class = "egg"
-if () {
-
+if (age_class == "egg") {
+  y <- 10
 }
 y
-
+#y = 10
 # 2. Complete the following if statement so that if age_class is equal to “egg”
 # it sets y <- 10 and if age_class is equal to “larva” it sets y <- 5.
 
 age_class = "larva"
-if () {
-
+if (age_class == "egg") {
+  y<-10
+} else if (age_class == "larva"){
+  y<-5
 }
 y
-
+#y=5
 # 3. Complete the following if statement so that if age_class is equal to “egg”
 # it sets y <- 10 and if age_class is equal to “larva” it sets y <- 5 and if
 # age_class is something else then it sets the value of y <- 0.
 
 age_class = "adult"
-if () {
-
+if (age_class == "egg") {
+  y<-10
+} else if (age_class == "larva"){
+  y<-5
+} else {
+  y<-0
 }
 y
+#y=0
 
 # 4. Convert your if/else if/ else statement from (3) into a function that takes
 # age_class as an argument and returns y. Call this function 4 times, once with
 # each of the following values for age_class: “egg”, “larva”, “adult”,
 # “senescent”.
-
+life_stage_value <- function(age_class){
+  if (age_class == "egg") {
+    y<-10
+  } else if (age_class == "larva"){
+    y<-5
+  } else {
+    y<-0
+  }
+  print(y)
+}
+life_stage_value("egg") #10
+life_stage_value("larva") #5
+life_stage_value("adult") #0
+life_stage_value("senescent") #0
 
 # Repeating yourself with vectorized functions ----------------------------
 
@@ -68,10 +107,10 @@ y
 # just their first and last characters. Verify it works on the names of sharks
 # below.
 sharks <- c("thresher", "mako", "tiger", "hammerhead")
-shorten_string <- function(???) {
-  first_letter <- substr(???, 1, 1)
+shorten_string <- function(x) {
+  first_letter <- substr(x, 1, 1)
   # Hint: nchar() returns the number of characters in a string
-  last_letter <- substr(???, nchar(???), nchar(???))
+  last_letter <- substr(x, nchar(x), nchar(x))
   result <- paste(first_letter, last_letter, sep = "")
   return(result)
 }
@@ -85,14 +124,22 @@ shorten_string(sharks)
 
 # 2. a and b for a shortfin mako are estimated to be 5.243e-6 and 3.141. What
 # are the estimated masses of sharks that are 100, 200, and 300 cm long?
-a <- ???
-b <- ???
-??? * c(???)^???
+a <- 5.243e-6
+b <- 3.141
+a* c(100, 200, 300)^b
 
 # 3. Write a function called shortfin_mako_mass_kg that takes a parameter L_cm
 # and returns the estimated masses of shortfin makos with fork lengths L_cm. Use
 # your function to answer question 2.
-
+shortfin_mako_mass_kg <- function(L_cm){
+  a <- 5.243e-6
+  b <- 3.141
+  mass <- a* c(L_cm)^b
+  print(mass)
+}
+shortfin_mako_mass_kg(100) #10.03644
+shortfin_mako_mass_kg(200) #88.53502
+shortfin_mako_mass_kg(300) #316.3863
 
 # Repeating yourself with for loops ---------------------------------------
 
@@ -101,30 +148,35 @@ b <- ???
 
 numbers <- c(1, 2, 3, 4, 5)
 for (number in numbers) {
-  print(number)
+  product <- number *3
+  print(product)
 }
 
 # 2. Write a for loop that loops over the following vector and prints out the
 # mass in kilograms (mass_kg = 2.2 * mass_lb)
 
 mass_lbs <- c(2.2, 3.5, 9.6, 1.2)
+for (number in mass_lbs) {
+  mass_kg <- 2.2 * number
+  print(mass_kg)
+}
 
 # 3. Complete the code below so that it prints out the name of each bird one
 # line at a time.
 
 birds = c("albatross", "puffin", "pelican", "penguin")
-for (i in 1:length(???)) {
-  print(birds[???])
+for (i in 1:length(birds)) {
+  print(birds[i])
 }
 
 # 4. Complete the code below so that it stores one area for each radius.
 
 radius <- c(1.3, 2.1, 3.5)
 areas <- rep(0, length(radius))
-for (??? in 1:length(???)) {
-  areas[???] <- pi * radius[i] ^ 2
+for (i in 1:length(radius)) {
+  areas[i] <- pi * radius[i] ^ 2
 }
-areas
+areas #5.309292 13.854424 38.484510
 
 # 5. Write a for loop that loops over the following vector and stores the height
 # in meters (height_m = height_ft / 3.28) in a new vector. After the for loop
@@ -132,6 +184,11 @@ areas
 # its own line.
 
 height_ft <- c(5.1, 6.3, 5.7, 5.4)
+height_m <- rep(0, length(height_ft))
+for (i in 1:length(height_ft)) {
+  height_m[i] <- height_ft[i]/3.82
+}
+height_m #1.335079 1.649215 1.492147 1.413613
 
 # 6. Complete the code below to calculate an area for each pair of lengths and
 # widths, store the areas in a vector, and after they are all calculated print
@@ -139,8 +196,9 @@ height_ft <- c(5.1, 6.3, 5.7, 5.4)
 
 lengths = c(1.1, 2.2, 1.6)
 widths = c(3.5, 2.4, 2.8)
-areas <- rep(0, ???)
-for (i in ???) {
-  areas[???] <- lengths[???] * widths[???]
+areas <- rep(0, length(lengths))
+for (i in 1:length(lengths)) {
+  areas[i] <- lengths[i] * widths[i]
 }
 areas
+# 3.85 5.28 4.48
